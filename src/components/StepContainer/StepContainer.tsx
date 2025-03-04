6db40ed949578de6722c95cfa8647578181bc41a
Nehuen Cuenca <nehuen.cuenca@hotmail.com>
2025-03-04 12:51:55 -0300

    Add conditional rendering to the headings of StepContainer

--- src/components/StepContainer/StepContainer.tsx
+++ src/components/StepContainer/StepContainer.tsx
@@ -5,13 +5,20 @@ interface StepContainerProps{
   containerTitle: string;
   containerSubTitle: string;
   isVisible: boolean;
+  hideContainerHeadings?: boolean
 }
 
-const StepContainer = ({children, containerTitle, containerSubTitle, isVisible}: StepContainerProps) => {
+const StepContainer = ({children, containerTitle, containerSubTitle, isVisible, hideContainerHeadings}: StepContainerProps) => {
   return (
     <div className={`step-container ${(!isVisible) ? 'step-container_hidden':''}`}>
-      <h1 className='step-title'>{containerTitle}</h1>
-      <h2 className='step-sub-title'>{containerSubTitle}</h2>
+      {
+        !hideContainerHeadings &&
+        <>
+          <h1 className='step-title'>{containerTitle}</h1>
+          <h2 className='step-sub-title'>{containerSubTitle}</h2>
+        </>
+      }
+      
       {children}
     </div>
   )
