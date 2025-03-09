08b98b6dc5d6f0aecc97936b6469572a3492addd
Nehuen Cuenca <nehuen.cuenca@hotmail.com>
2025-03-09 20:35:45 -0300

    Add path aliases for the folders that are frequently used

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./"),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@ownTypes': path.resolve(__dirname, './src/types'),
+      '@data': path.resolve(__dirname, './src/data'),
+      '@starterFiles': path.resolve(__dirname, './starter_files'),
+    },
+  },
 })
